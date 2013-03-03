dd1eadcdbffcf403828b50664fa1440a0b096367
Juha Kajava <juhakaja@gmail.com>
2013-03-03 21:48:10 +0900

    Init.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rcast::Application.config.secret_token = '69d5c9809d0ac4242d24c256692f94e2a8374c14e69a0810868d769620e82a28a8df32de7661a16ea400ddc4ec25402b652369d375f43845c02c184c950a3928'
